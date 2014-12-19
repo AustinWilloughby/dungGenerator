1683cf1cc464057a8d2fb136bb15b542dca70ed9
Austin Willoughby <arw8020@rit.edu>
2014-12-18 21:37:23 -0500

    Unity Project Added
    New unity2d project added, character controllable square for testing

---
+++ Assets/CharacterMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour {
+    //Fields
+    private float speed = 1.0f;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float xMovement = Input.GetAxis("Horizontal") * speed;
+        float yMovement = Input.GetAxis("Vertical") * speed;
+
+        Vector3 translate = transform.position;
+        translate.x += xMovement;
+        translate.y += yMovement;
+
+        transform.position = translate;
+
+	}
+}
