6ad091b21ea07e061c85c3b7936535f2a11d1616
Austin Willoughby <arw8020@rit.edu>
2014-12-19 17:22:08 -0500

    Enemy Seeking Improvements
    Extended the enemy's seeking behavior into an arrival behavior. Also
    made the enemies not stack on top of each other.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,34 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyScript : MonoBehaviour 
+public class EnemyScript : MonoBehaviour
 {
-    public float viewDistance;
-    public float speed;
+    public float viewDistance = 10f;
+    public float speed = .05f;
     private GameObject player;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
-        viewDistance = 10f;
-        speed = .005f;
         player = GameObject.FindGameObjectWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (Vector3.Distance(transform.position, player.transform.position) < viewDistance)
         {
-            transform.position += Seek(player);
+            transform.position += Arrive(player, 3);
         }
-	}
+    }
 
-    Vector3 Seek(GameObject target)
+    Vector3 Seek(GameObject target) //Basic Seeking Behavior
     {
         Vector3 seek = Vector3.zero;
         seek = target.transform.position - transform.position;
-        Vector3.ClampMagnitude(seek, speed*Time.deltaTime);
+        seek = Vector3.Normalize(seek);
+        seek = seek * speed;
         return seek;
+    }
+
+    Vector3 Arrive(GameObject target, float slowRadius) //Advanced seeking with slow down at target
+    {
+        Vector3 arrive = Seek(target);
+        if (Vector3.Distance(transform.position, target.transform.position) < slowRadius)
+        {
+            arrive = Vector3.Normalize(arrive);
+            arrive = arrive * speed;
+            arrive = arrive * ((Vector3.Distance(transform.position, player.transform.position) - 1.5f) / slowRadius);
+        }
+        return arrive;
     }
 }
