130583529b0b72f892e5d21a74316cafc059dd5d
Austin Willoughby <arw8020@rit.edu>
2014-12-22 17:54:53 -0500

    Collectables implemented
    Collectable script implemented. Currently coins with a value of 1 have
    been added, but the script is fully equipped to handle any kind of
    collectable of any value.

---
+++ Assets/Scripts/CollectableScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableScript : MonoBehaviour {
+
+    private GameObject player;
+    public int value = 1; //Value of the collectable
+
+	// Use this for initialization
+	void Start () 
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    void OnTriggerEnter2D(Collider2D other) //Triggers when a 2D collider intersects coin collider
+    {
+        if (other.gameObject == player) //If the gameobject is the player, then collect and destroy
+        {
+            player.GetComponent<StatTracker>().purseValue += value;
+            GameObject.Destroy(gameObject);
+        }
+
+    }
+}
--- Assets/Scripts/StatTracker.cs
+++ Assets/Scripts/StatTracker.cs
@@ -6,4 +6,5 @@ public class StatTracker : MonoBehaviour
     //Fields
     public int health = 10;
     public int energy = 10;
+    public int purseValue = 0;
 }
