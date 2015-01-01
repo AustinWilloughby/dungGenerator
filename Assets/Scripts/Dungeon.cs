ff10d5c2d5ec71aeb19517b18c77372f058b98d6
Austin Willoughby <arw8020@rit.edu>
2015-01-01 18:05:42 -0500

    Basic Generation Started
    Implemented a system that generates a grid of cells. This grid will be
    refined into a dungeon using a random walk algorithm.

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dungeon : MonoBehaviour
+{
+    //Fields
+    //Public
+    public int xSize;
+    public int ySize;
+    public float cellScale;
+    public DungeonCell cellPrefab;
+
+    //Private
+    private DungeonCell[,] cells;
+
+    //Events
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Methods
+    public void Generate()
+    {
+        cells = new DungeonCell[xSize, ySize];
+        print("yolo");
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                CreateCell(x, y);
+            }
+        }
+    }
+
+    public void CreateCell(int x, int y)
+    {
+        DungeonCell tempCell = Instantiate(cellPrefab) as DungeonCell;
+        cells[x, y] = tempCell;
+        tempCell.name = "Maze Cell " + x + ", " + y;
+        tempCell.transform.parent = transform;
+        tempCell.transform.localPosition = new Vector3(x * cellScale, y * cellScale, 0);
+    }
+}
---
+++ Assets/Scripts/DungeonGeneration.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonGeneration : MonoBehaviour
+{
+    //Fields
+    //Public
+    public Dungeon dungeonPrefab;
+
+    //Private
+    private Dungeon dungeonInstance;
+
+    //Events
+    // Use this for initialization
+    void Start()
+    {
+        Begin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Methods
+    private void Begin()
+    {
+        dungeonInstance = Instantiate(dungeonPrefab) as Dungeon;
+        dungeonInstance.Generate();
+    }
+}
