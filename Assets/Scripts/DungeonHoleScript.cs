6d9fd6279dc97735a7da33e0d78cac9318f4e171
Austin Willoughby <arw8020@rit.edu>
2015-01-09 23:58:48 -0500

    Basic Rope and Hole Scripting
    Basic scripts for the dungeon hole and rope have been implemented. If
    the player collects the rope, the dungeon hole will be passable.

---
+++ Assets/Scripts/DungeonHoleScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonHoleScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) //Triggers when a 2D collider intersects DungeonHole collider
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Generate new level
+        }
+    }
+}
---
+++ Assets/Scripts/RopeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeScript : MonoBehaviour
+{
+    //Fields
+    //Public
+    public GameObject ropelessHole;
+    public GameObject ropeHolePrefab;
+
+    void OnTriggerEnter2D(Collider2D other) //Triggers when a 2D collider intersects rope
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject ropeHole = (GameObject)Instantiate(ropeHolePrefab);
+            ropeHole.transform.position = ropelessHole.transform.position;
+            GameObject.Destroy(ropelessHole);
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
