4437af85cdd7aaabb5de1129611789c84d0eaaab
Austin Willoughby <arw8020@rit.edu>
2015-01-22 18:35:52 -0500

    Attempt At Main Menu
    Began attempting a main menu. Currently non functional.

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //Fields
+    //Public
+    public MenuElement button;
+
+    //Events
+    void OnMouseEnter()
+    {
+        guiText.material.color = Color.black;
+    }
+
+    void OnMouseExit()
+    {
+        guiText.material.color = Color.red;
+    }
+
+    void OnMouseUpAsButton()
+    {
+        switch (button)
+        {
+            case MenuElement.Options:
+                break;
+
+            case MenuElement.Start:
+                Application.LoadLevel(1);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
+
+public enum MenuElement
+{
+    Start,
+    Options,
+};
