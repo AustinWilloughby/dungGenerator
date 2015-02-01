1810c8115ffbad88418453b7d3eeee6c471d82e1
Austin Willoughby <arw8020@rit.edu>
2015-02-01 13:30:01 -0500

    Realistic Torch Light Flicker
    Began making the spotlight flicker to represent a torch. Needs
    improvement but it is a start.

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour
+{
+    //Fields
+    //Private
+    private GameObject player;
+    private GameObject boss;
+    private float distanceToBoss;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        boss = GameObject.Find("Boss");
+        distanceToBoss = Vector2.Distance((Vector2)player.transform.position, (Vector2)boss.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToBoss = Vector2.Distance((Vector2)player.transform.position, (Vector2)boss.transform.position);
+        Flicker();
+    }
+
+    private void Flicker()
+    {
+        if (distanceToBoss > 15f)
+        {
+            float noise = Mathf.PerlinNoise(Random.Range(0.0f, 65535.0f), Time.time / 5f);
+            light.intensity = Mathf.Lerp(.8f, 1.1f, noise);
+        }
+        else
+        {
+            light.spotAngle = distanceToBoss / 1.2f;
+        }
+    }
+
+
+}
