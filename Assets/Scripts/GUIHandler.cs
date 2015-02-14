364ffd8c400cc077afc0ea11433260bd813d7058
Austin Willoughby <arw8020@rit.edu>
2015-02-14 12:21:08 -0500

    Fixed 0 Heath Display
    Made 0 health display properly. Currently not visible, but when the game
    freezes after you die, rather than respawning, it will be obvious.

--- Assets/Scripts/GUIHandler.cs
+++ Assets/Scripts/GUIHandler.cs
@@ -23,27 +23,37 @@ public class GUIHandler : MonoBehaviour
 
     private void DrawHearts()
     {
-                //Draws players health to the screen
-        int heartsDrawn = 0;
-        for (int i = 0; i < playerStats.health / 2; i++)
+        //Draws players health to the screen
+        if (playerStats.health > 0)
         {
-            GUI.DrawTexture(new Rect(10 + 65 * heartsDrawn, 10, 60, 60), heartTypes[0], ScaleMode.StretchToFill, true);
-            heartsDrawn++;
+            int heartsDrawn = 0;
+            for (int i = 0; i < playerStats.health / 2; i++)
+            {
+                GUI.DrawTexture(new Rect(10 + 65 * heartsDrawn, 10, 60, 60), heartTypes[0], ScaleMode.StretchToFill, true);
+                heartsDrawn++;
+            }
+            if (playerStats.health / 2 < ((float)(playerStats.health) / 2f))
+            {
+                GUI.DrawTexture(new Rect(10 + 65 * heartsDrawn, 10, 60, 60), heartTypes[1], ScaleMode.StretchToFill, true);
+                heartsDrawn++;
+            }
+            int emptyHearts = playerStats.MaxHealth - playerStats.health;
+            if ((emptyHearts) % 2 == 1)
+            {
+                emptyHearts--;
+            }
+            for (int i = 0; i < emptyHearts / 2; i++)
+            {
+                GUI.DrawTexture(new Rect(10 + 65 * heartsDrawn, 10, 60, 60), heartTypes[2], ScaleMode.StretchToFill, true);
+                heartsDrawn++;
+            }
         }
-        if (playerStats.health / 2 < ((float)(playerStats.health) / 2f))
+        else
         {
-            GUI.DrawTexture(new Rect(10 + 65 * heartsDrawn, 10, 60, 60), heartTypes[1], ScaleMode.StretchToFill, true);
-            heartsDrawn++;
-        }
-        int emptyHearts = playerStats.MaxHealth - playerStats.health;
-        if((emptyHearts)%2 == 1)
-        {
-            emptyHearts--;
-        }
-        for (int i = 0; i < emptyHearts / 2; i++)
-        {
-            GUI.DrawTexture(new Rect(10 + 65 * heartsDrawn, 10, 60, 60), heartTypes[2], ScaleMode.StretchToFill, true);
-            heartsDrawn++;
+            for (int i = 0; i < playerStats.MaxHealth / 2; i++)
+            {
+                GUI.DrawTexture(new Rect(10 + 65 * i, 10, 60, 60), heartTypes[2], ScaleMode.StretchToFill, true);
+            }
         }
     }
 }
