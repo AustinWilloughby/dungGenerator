f722bb740180d4c8b3632051a1583e6be7f59fd2
Austin Willoughby <arw8020@rit.edu>
2015-02-19 13:58:06 -0500

    New Inventory Implementation Started
    Began the new OnGUI inventory implementation. Added graphics and basic
    script to handle coin counting.

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryManager : MonoBehaviour
+{
+    //Fields
+    //Public
+
+    //Private
+    private int coinCount;
+    private GameObject[] inventory;
+    private int[] objectCount;
+
+
+    //Properties
+    public int CoinCount
+    {
+        get { return coinCount; }
+    }
+
+
+    //Events
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //Methods
+    public void AddCoinValue(int addValue)
+    {
+        if (addValue >= 0)
+        {
+            coinCount += addValue;
+        }
+        else
+        {
+            coinCount -= addValue;
+        }
+    }
+}
