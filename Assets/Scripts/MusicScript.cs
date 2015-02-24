fb8e45cdf4504ae4fdc903c13f022b0d87290a58
Austin Willoughby <arw8020@rit.edu>
2015-02-24 15:11:53 -0500

    Beginning Music Implementation
    The base of the music and room tone implementation has been started.

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour
+{
+    //Fields
+    //Public
+    public AudioSource[] backgroundMusic;
+    public AudioSource roomTone;
+
+    //Private
+    private GameObject pause;
+    private bool lastFramePause;
+    private int songIndex;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        songIndex = 1;
+        lastFramePause = true;
+        pause = GameObject.Find("PauseMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayMusic();
+        PlayRoomTone();
+    }
+
+    //Methods
+    private void PlayMusic()
+    {
+        if (pause.renderer.enabled)
+        {
+            if (backgroundMusic[songIndex].isPlaying)
+            {
+                backgroundMusic[songIndex].Pause();
+            }
+        }
+        if (lastFramePause && !pause.renderer.enabled)
+        {
+            backgroundMusic[songIndex].Play();
+        }
+
+        lastFramePause = pause.renderer.enabled;
+    }
+
+    private void PlayRoomTone()
+    {
+        if (!roomTone.isPlaying)
+        {
+            if (Random.Range(0, 100) == 0)
+            {
+                roomTone.Play();
+            }
+        }
+    }
+}
