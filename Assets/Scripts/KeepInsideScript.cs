6cf1320fd80fb3e5f2e4d26253198486af8d2c41
Austin Willoughby <arw8020@rit.edu>
2015-03-11 15:35:40 -0400

    Fixed Outside Of Dungeon Issue
    Now if the player is pushed outside the dungeon, they are teleported
    back to the "spawn" point so that they can continue playing. Even if it
    is inconvenient, it's better than not being able to play.

---
+++ Assets/Scripts/KeepInsideScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepInsideScript : MonoBehaviour
+{
+    //Fields
+    //Private
+    private GameObject player;
+    private Dungeon dungeon;
+    private GameObject playerSpawn;
+
+    //Events
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerSpawn = GameObject.Find("PlayerSpawn");
+        dungeon = gameObject.GetComponent<Dungeon>();
+        gameObject.GetComponent<BoxCollider2D>().size = new Vector2(dungeon.size.x, dungeon.size.y);
+        gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(dungeon.size.x / 2 - .5f, dungeon.size.y / 2 - .5f);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //If the player leaves the overall dungeon collider he gets sent back to the spawn
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.position = (Vector2)playerSpawn.transform.position;
+        }
+    }
+}
