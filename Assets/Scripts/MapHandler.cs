e486d124ccd96cacdd2c4025b1d2fb4e17661fe2
Austin Willoughby <arw8020@rit.edu>
2015-03-28 18:54:59 -0400

    Commenting
    Commented new code to keep project entirely commented.

--- Assets/Scripts/MapHandler.cs
+++ Assets/Scripts/MapHandler.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class MapHandler : MonoBehaviour
 {
     //Fields
+    //Private
     private List<DungeonCell> visitedCells;
     private GameObject player;
     private Dungeon dungeon;
@@ -22,7 +23,7 @@ public class MapHandler : MonoBehaviour
         CheckCurrentCell();
     }
 
-    private void CheckCurrentCell()
+    private void CheckCurrentCell() //Determines the current player cell and adds it to the list of visited cells if needed
     {
         dungeon = GameObject.Find("Dungeon(Clone)").GetComponent<Dungeon>();
         IntVector2 playerCoords = new IntVector2((int)((player.transform.position.x + (dungeon.cellScale / 2)) / dungeon.cellScale), 
